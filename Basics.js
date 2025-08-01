151b3c4aa4b9cf3218c0b2dfbc198d8138966ba4
S-Q-Ali <syedqasim963@gmail.com>
2025-08-01 08:39:52 -0700

    Use of forEach filter and map

--- Basics.js
+++ Basics.js
@@ -222,3 +222,19 @@ AccessObjectProperties();
 console.log("Hello World");
 console.log(`Hello
     World`);
+
+
+const veggies = ['onion', 'garlic', 'potato'];
+veggies.forEach( function(veggie, index) {
+    console.log(`${index}. ${veggie}`);
+});
+
+
+const nums = [0,10,20,30,40,50].filter( function (num) {
+    return num > 20;
+})
+console.log(nums);
+const arr=[0,10,20,30,40,50].map( function(num) {
+    return num / 10
+})
+console.log(arr);
